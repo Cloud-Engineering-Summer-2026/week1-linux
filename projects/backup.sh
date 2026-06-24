caed7fe9fd33e14e721375682df24a195a9fde67
micah1of1 <yallknowmicah@gmail.com>
2026-06-24 11:52:56 +0300

    feat: backup.sh automated timestamped backup with logging

--- projects/backup.sh
+++ projects/backup.sh
@@ -4,23 +4,35 @@
 # Week 1 - Cloud Accelerator
 # Author: Micah Ade Cedar
 
-# Path of the directory we wish to back up
-SOURCE=$1
+# Setting strict mode
+set -euo pipefail
+# set -e -> exit immediately if any command fails
+# set -u -> treat unset variables as errors
+# set -o pipefail -> catch errors on both sides of pipes not just end
 
-# Path of where we wish to store the directory
-DEST=$2
-
+LOG_FILE="$HOME/backup.log"
 # Timestamp generation, down to the second, to differentiate back ups
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 # Build filename for backup archive
 BACKUP_NAME="backup_${TIMESTAMP}.tar.gz"
 
-# Validate if paths were provided
-if [ -z "$SOURCE" ] || [ -z "$DEST" ]; then
+#-----Guard clauses---------------------------------------------------
+# Check if root user
+if [ $EUID -eq 0 ]; then
+	echo "Do not run backup as root."
+	exit 1
+fi
+
+#Validate arguements, with fallback modifier to avoid ugly error
+if [ -z "${1:-}" ] || [ -z "${2:-}" ]; then
 	echo "Usage: ./backup.sh <source_directory> <destination_directory>"
 	exit 1
 fi
+
+# Arguement passing
+SOURCE=$1
+DEST=$2
 
 # Ensure source directory is real
 if [ ! -d "$SOURCE" ]; then
@@ -28,12 +40,14 @@ if [ ! -d "$SOURCE" ]; then
 	exit 1
 fi
 
-# Robust destination path creation and validation
+#------Backup-----------------------------------------------------------
+# Robust destination directory creation
 mkdir -p "$DEST"
 
 # Backup file creation
 tar -czf "$DEST/$BACKUP_NAME" "$SOURCE" && \
-echo "$(date): Backed up '$SOURCE' to '$DEST/$BACKUP_NAME'" >> ~/backup.log
+echo "$(date): Backed up '$SOURCE' to '$DEST/$BACKUP_NAME'" >> "$LOG_FILE"
 
-echo "Backup complete: $DEST/$BACKUP_NAME"
+echo "✓ Backup complete: $DEST/$BACKUP_NAME"
+echo "✓ Logged to $LOG_FILE"
 
